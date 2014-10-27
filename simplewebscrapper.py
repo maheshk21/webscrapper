bf84b4bb775e1a3ede47b7dbf6a33bb64015aca6
Mahesh Kumar <maheshkumar.wdp21@gmail.com>
2014-10-27 23:29:25 +0530

    Update simplewebscrapper.py

--- simplewebscrapper.py
+++ simplewebscrapper.py
@@ -6,10 +6,10 @@ from bs4 import BeautifulSoup
 import requests
 
 def webscrap():
-	url = raw_input("Enter the url:\t")
-	link = requests.get('http://'+url)
-	data = link.text
-	soup = BeautifulSoup(data)
+	url = raw_input("Enter the url:\t")  # accepts the url
+	link = requests.get('http://'+url)   # it requests for that particular url
+	data = link.text                     # converting into into
+	soup = BeautifulSoup(data)           # Parsing the data using BeautifulSoup
 	for link in soup.find_all('a'):
 		print(link.get('href'))
 
