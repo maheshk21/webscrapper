31f80244034591cea32b8b3d2d02286640777aee
Mahesh Kumar K <maheshkumar.wdp21@gmail.com>
2014-10-31 00:38:35 +0530

    modified code

--- coursera.py
+++ coursera.py
@@ -4,16 +4,31 @@
 import requests
 import json
 
-def nyscrapper():
-	url = 'https://api.coursera.org/api/catalog.v1/courses'
-	response = requests.get(url)
-	#response.text	
-	jsondata = json.loads(response.text)
-	for item in jsondata['elements']:
+def courseradata():
+	# api links
+	courseurl = 'https://api.coursera.org/api/catalog.v1/courses'
+	universitiesurl='https://api.coursera.org/api/catalog.v1/universities'
+	
+	# access the corresponding links	
+	cresponse = requests.get(courseurl)
+	uresponse = requests.get(universitiesurl)
+
+	# loading the data into json format	
+	cjsondata = json.loads(cresponse.text)
+	ujsondata = json.loads(uresponse.text)
+	
+	# for loop to print available courses
+	for item in cjsondata['elements']:
 		print "Course ID:",item['id']
 		print "Course Short Name:",item['shortName']
 		print "Course Name:",item['name']
 		print "\n"
 
+	# for loop to print participating universities	
+	for item in ujsondata['elements']:
+		print "University ID:", item['id']
+		print "University Name:", item['shortName']
+		print "\n"
+
 if __name__=="__main__":
-	nyscrapper()
+	courseradata()
