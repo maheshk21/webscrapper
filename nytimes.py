caae26f4183dcc10e85482394b379030c14f6304
Mahesh Kumar K <maheshkumar.wdp21@gmail.com>
2014-10-30 23:46:43 +0530

    nytimes

---
+++ nytimes.py
@@ -0,0 +1,20 @@
+# Web Scrapping program to access news on The New York Times
+# Author : Mahesh Kumar K
+
+import requests
+import lxml.html as lh
+
+def nyscrapper():
+	url = str(raw_input("Enter the NY Times link :\t"))
+	response = requests.get(url)
+	document = lh.fromstring(response.content)
+	text = document.xpath('//p[@itemprop="articleBody"]')
+	nynews = str()
+
+	for details in text:
+		nynews += details.text_content()
+
+	print nynews
+
+if __name__=="__main__":
+	nyscrapper()
